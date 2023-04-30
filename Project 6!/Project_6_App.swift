89d8c97ab6a6b6f08484f0b8d253045018ef416d
Ayden King <king.ayden123@gmail.com>
2023-04-29 20:08:44 -0400

    Initial Commit

---
+++ Project 6!/Project_6_App.swift
@@ -0,0 +1,17 @@
+//
+//  Project_6_App.swift
+//  Project 6!
+//
+//  Created by Loaner on 4/29/23.
+//
+
+import SwiftUI
+
+@main
+struct Project_6_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
